cf5d9a1233e3caa4862660c117ba992ecdb493f5
Cheka <39255806+Argentix03@users.noreply.github.com>
2022-09-27 06:03:36 +0300

    Update DebuggerCheckCloseHandle.cpp

--- DebuggerCheckCloseHandle.cpp
+++ DebuggerCheckCloseHandle.cpp
@@ -5,7 +5,6 @@ int main(int argc, const char* argv[])
 {
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, 4);
     SetHandleInformation(hProcess, HANDLE_FLAG_PROTECT_FROM_CLOSE, HANDLE_FLAG_PROTECT_FROM_CLOSE);
-    MessageBox(nullptr, L"SetHandleInformation(hProcess, HANDLE_FLAG_PROTECT_FROM_CLOSE, HANDLE_FLAG_PROTECT_FROM_CLOSE);", L"handle information set", MB_OK);
     __try {
         if (!CloseHandle(hProcess)) {
             printf("Failed to close handle\n");
